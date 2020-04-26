7f669da0e182da68e6aed5c0ab7519d8dd4f5c00
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-04-26 15:17:58 -0400

    add forest creation

--- huffman.go
+++ huffman.go
@@ -1,11 +1,33 @@
 package huffman
 
-func charCount(content string) map[rune]int {
+type tree struct {
+	char   rune
+	weight int
+	left   *tree
+	right  *tree
+}
+
+func countChar(content string) map[rune]int {
 	count := make(map[rune]int)
 	for _, v := range content {
 		count[v]++
 	}
 	return count
+}
+
+func createForest(charCount map[rune]int) []tree {
+	var numChars int
+	for range charCount {
+		numChars++
+	}
+	forest := make([]tree, numChars)
+	for _, tree := range forest {
+		for c, v := range charCount {
+			tree.char = c
+			tree.weight = v
+		}
+	}
+	return forest
 }
 
 func main() {
