16d59035235abd06a9916f781eee4fa04f16bdab
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-04-27 15:18:39 -0400

    Finish forest creation and testing and start implementing combination into single tree

--- huffman.go
+++ huffman.go
@@ -1,4 +1,6 @@
-package huffman
+package main
+
+import "fmt"
 
 type tree struct {
 	char   rune
@@ -7,7 +9,7 @@ type tree struct {
 	right  *tree
 }
 
-func countChar(content string) map[rune]int {
+func countChars(content string) map[rune]int {
 	count := make(map[rune]int)
 	for _, v := range content {
 		count[v]++
@@ -15,21 +17,57 @@ func countChar(content string) map[rune]int {
 	return count
 }
 
-func createForest(charCount map[rune]int) []tree {
+func makeForest(charCount map[rune]int) []tree {
 	var numChars int
 	for range charCount {
 		numChars++
 	}
+	//fmt.Println("number of characters: ", numChars)
 	forest := make([]tree, numChars)
-	for _, tree := range forest {
-		for c, v := range charCount {
-			tree.char = c
-			tree.weight = v
-		}
+	i := 0
+	for c, v := range charCount {
+		forest[i].char = c
+		forest[i].weight = v
+		fmt.Println(forest[i])
+		i++
 	}
 	return forest
 }
 
-func main() {
+func combineTrees(t1 tree, t2 tree) tree {
+	var t3 tree
+	t3.weight = t1.weight + t2.weight
+	t3.left = &t1
+	t3.right = &t2
+	return t3
+}
 
+func makeTree(forest []tree) tree {
+	var lowestWeight1, lowestWeight2, lw1I, lw2I, treeCount int = forest[0].weight, forest[1].weight, 0, 1, len(forest)
+	var t3 tree
+	for treeCount > 1 {
+		for i, t := range forest {
+			if t.weight != 0 && t.weight < lowestWeight1 {
+				lowestWeight2 = lowestWeight1
+				lw2I = lw1I
+				lowestWeight1 = t.weight
+				lw1I = i
+			}
+		}
+		t3 = combineTrees(forest[lw1I], forest[lw2I])
+		forest[lw1I] = t3
+		leftSlice := forest[:lw2I]
+		rightSlice := forest[lw2I+1:]
+		forest = append(leftSlice, rightSlice)
+		treeCount = len(forest)
+	}
+	return t3
+}
+
+func main() {
+	content := "go go gophers"
+	charCount := countChars(content)
+	fmt.Println(charCount)
+	forest := makeForest(charCount)
+	fmt.Println(forest)
 }
--- huffman_test.go
+++ huffman_test.go
@@ -1,6 +1,7 @@
-package huffman
+package main
 
 import (
+	"fmt"
 	"log"
 	"testing"
 )
@@ -40,12 +41,82 @@ func TestCharCount(t *testing.T) {
 	}
 	for _, ex := range examples {
 		t.Run(ex.name, func(t *testing.T) {
-			got := charCount(ex.s)
+			got := countChars(ex.s)
 			for k, v := range got {
 				if got[k] != ex.want[k] {
 					log.Fatalf("got %v for %c. want %v\n", v, k, ex.want[k])
 				}
 			}
+		})
+	}
+}
+
+func TestmakeForest(t *testing.T) {
+	examples := []struct {
+		name   string
+		counts map[rune]int
+		want   []tree
+	}{
+		{
+			name: "go go gophers",
+			counts: map[rune]int{
+				32:  2,
+				101: 1,
+				103: 3,
+				104: 1,
+				111: 3,
+				112: 1,
+				114: 1,
+				115: 1,
+			},
+			want: []tree{
+				{32, 2, nil, nil},
+				{101, 1, nil, nil},
+				{103, 3, nil, nil},
+				{104, 1, nil, nil},
+				{111, 3, nil, nil},
+				{112, 1, nil, nil},
+				{114, 1, nil, nil},
+				{115, 1, nil, nil},
+			},
+		},
+	}
+	for _, ex := range examples {
+		t.Run(ex.name, func(t *testing.T) {
+			forest := makeForest(ex.counts)
+			fmt.Println(forest)
+			gotMap := make(map[rune]int)
+			for _, tree := range forest {
+				gotMap[tree.char] = tree.weight
+			}
+			fmt.Println(gotMap)
+			for i := 0; i < len(ex.want); i++ {
+				count, ok := gotMap[ex.want[i].char]
+				if ok != false {
+					if count != ex.want[i].weight {
+						log.Fatalf("got %v, want %v", count, ex.want[i].weight)
+					}
+				} else {
+					log.Fatalf("character '%c' missing from created forest. value: %v", ex.want[i].char, ex.want[i].char)
+				}
+			}
+
+			/*
+				for i := 0; i < len(forest); i++ {
+					count, ok := gotMap[forest[i].char]
+					if ok != false {
+						if count != forest[i].weight {
+							log.Fatalf("got %v, want %v", )
+						}
+					}
+					for c, count := range gotMap {
+
+					}
+					if forest[i] != ex.want[i] {
+						log.Fatalf("got %v, want %v for entry %v\n", got[i], ex.want[i], i)
+					}
+				}
+			*/
 		})
 	}
 }
