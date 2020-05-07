c29d3afe7ecf640816812891ab6747d940f46dcf
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-05-07 11:36:17 -0400

    Finish single tree creation

--- huffman.go
+++ huffman.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type tree struct {
 	char   rune
@@ -43,9 +46,23 @@ func combineTrees(t1 tree, t2 tree) tree {
 }
 
 func makeTree(forest []tree) tree {
+	var newTree tree
+	for len(forest) > 1 {
+		sort.Slice(forest, func(i, j int) bool { return forest[i].weight < forest[j].weight })
+		newTree = combineTrees(forest[0], forest[1])
+		forest[1] = newTree
+		forest = forest[1:]
+	}
+	return newTree
+}
+
+/*
+//combine the two trees of lowest weight in a forest until there is only one tree remaining
+func makeTree(forest []tree) tree {
 	var lowestWeight1, lw1I, lw2I, treeCount int = forest[0].weight, 0, 1, len(forest)
 	var t3 tree
-	for treeCount > 1 {
+	for ; treeCount > 1; treeCount = len(forest) {
+		//find index of two trees with lowest weights
 		for i, t := range forest {
 			if t.weight != 0 && t.weight < lowestWeight1 {
 				lw2I = lw1I
@@ -54,6 +71,7 @@ func makeTree(forest []tree) tree {
 			}
 		}
 		t3 = combineTrees(forest[lw1I], forest[lw2I])
+		//insert the new combined tree into the forest and remove the now extraneous tree
 		forest[lw1I] = t3
 		leftSlice := forest[:lw2I]
 		if lw2I+1 < len(forest) {
@@ -62,13 +80,38 @@ func makeTree(forest []tree) tree {
 				leftSlice = append(leftSlice, rightSlice[i])
 			}
 		}
-		treeCount = len(forest)
+		forest = leftSlice
 	}
 	return t3
 }
 
+func traverse(t tree) []rune {
+	current := &t
+	if current.left != nil {
+
+	}
+}
+
+
 func makeTable(t tree) map[rune]byte {
 
+}
+*/
+
+func storeLeaves(root *tree) []rune {
+	s := make([]rune, 0)
+	if root != nil {
+		if root.left != nil {
+			s = append(s, storeLeaves(root.left)...)
+		}
+		if root.left == nil && root.right == nil {
+			s = append(s, root.char)
+		}
+		if root.right != nil {
+			s = append(s, storeLeaves(root.right)...)
+		}
+	}
+	return s
 }
 
 func main() {
@@ -77,4 +120,7 @@ func main() {
 	fmt.Println(charCount)
 	forest := makeForest(charCount)
 	fmt.Println(forest)
+	t := makeTree(forest)
+	s := storeLeaves(&t)
+	fmt.Println(s)
 }
