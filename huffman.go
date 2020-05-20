232ffcc93f84f8da74ad72dd3215e7c0d9f0e4d6
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-05-20 13:54:53 -0400

    implement path tracing function

--- huffman.go
+++ huffman.go
@@ -8,8 +8,8 @@ import (
 type tree struct {
 	char   rune
 	weight int
-	left   *tree
-	right  *tree
+	l      *tree
+	r      *tree
 }
 
 func countChars(content string) map[rune]int {
@@ -40,8 +40,8 @@ func makeForest(charCount map[rune]int) []tree {
 func combineTrees(t1 tree, t2 tree) tree {
 	var t3 tree
 	t3.weight = t1.weight + t2.weight
-	t3.left = &t1
-	t3.right = &t2
+	t3.l = &t1
+	t3.r = &t2
 	return t3
 }
 
@@ -56,71 +56,69 @@ func makeTree(forest []tree) tree {
 	return newTree
 }
 
-/*
-//combine the two trees of lowest weight in a forest until there is only one tree remaining
-func makeTree(forest []tree) tree {
-	var lowestWeight1, lw1I, lw2I, treeCount int = forest[0].weight, 0, 1, len(forest)
-	var t3 tree
-	for ; treeCount > 1; treeCount = len(forest) {
-		//find index of two trees with lowest weights
-		for i, t := range forest {
-			if t.weight != 0 && t.weight < lowestWeight1 {
-				lw2I = lw1I
-				lowestWeight1 = t.weight
-				lw1I = i
-			}
-		}
-		t3 = combineTrees(forest[lw1I], forest[lw2I])
-		//insert the new combined tree into the forest and remove the now extraneous tree
-		forest[lw1I] = t3
-		leftSlice := forest[:lw2I]
-		if lw2I+1 < len(forest) {
-			rightSlice := forest[lw2I+1:]
-			for i := 0; i < len(rightSlice); i++ {
-				leftSlice = append(leftSlice, rightSlice[i])
-			}
-		}
-		forest = leftSlice
-	}
-	return t3
-}
-
-func traverse(t tree) []rune {
-	current := &t
-	if current.left != nil {
-
-	}
-}
-
-
-func makeTable(t tree) map[rune]byte {
-
-}
-*/
-
 func storeLeaves(root *tree) []rune {
 	s := make([]rune, 0)
 	if root != nil {
-		if root.left != nil {
-			s = append(s, storeLeaves(root.left)...)
+		if root.l != nil {
+			s = append(s, storeLeaves(root.l)...)
 		}
-		if root.left == nil && root.right == nil {
+		if root.l == nil && root.r == nil {
 			s = append(s, root.char)
 		}
-		if root.right != nil {
-			s = append(s, storeLeaves(root.right)...)
+		if root.r != nil {
+			s = append(s, storeLeaves(root.r)...)
 		}
 	}
 	return s
 }
 
+//traverse tree and store each value in order in some data structure
+func traverse(root *tree) []rune {
+	//Left, Node, Right
+	s := make([]rune, 0)
+	if root != nil {
+		if root.l != nil {
+			s = append(s, traverse(root.l)...)
+		}
+		s = append(s, root.char)
+		if root.r != nil {
+			s = append(s, traverse(root.r)...)
+		}
+	}
+	return s
+}
+
+func paths(t *tree, path string) map[rune]string {
+	if t == nil {
+		return nil
+	}
+	result := merge(paths(t.l, path+"0"), paths(t.r, path+"1"))
+	if t.char != 0 {
+		result[t.char] = path
+	}
+	return result
+}
+
+func merge(maps ...map[rune]string) map[rune]string {
+	result := make(map[rune]string)
+	for _, m := range maps {
+		for char, path := range m {
+			result[char] = path
+		}
+	}
+	return result
+}
+
 func main() {
-	content := "bookkeeping"
+	content := "streets are stone stars are not"
 	charCount := countChars(content)
-	fmt.Println(charCount)
+	//fmt.Println(charCount)
 	forest := makeForest(charCount)
-	fmt.Println(forest)
+	//fmt.Println(forest)
 	t := makeTree(forest)
-	s := storeLeaves(&t)
-	fmt.Println(s)
+	var path string
+	table := paths(&t, path)
+	for c, p := range table {
+		fmt.Printf("%c: %s \n", c, p)
+	}
 }
