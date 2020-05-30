834d3c7be1d51aa7941b2621ac1e5fb0f39db303
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-05-30 16:09:44 -0400

    implement tree compression

--- huffman.go
+++ huffman.go
@@ -8,10 +8,11 @@ import (
 )
 
 type tree struct {
-	char   rune
-	weight int
-	l      *tree
-	r      *tree
+	id int
+	c  rune
+	w  int
+	l  *tree
+	r  *tree
 }
 
 func countChars(content string) map[rune]int {
@@ -31,31 +32,69 @@ func makeForest(charCount map[rune]int) []tree {
 	forest := make([]tree, numChars)
 	i := 0
 	for c, v := range charCount {
-		forest[i].char = c
-		forest[i].weight = v
-		fmt.Println(forest[i])
+		forest[i].c = c
+		forest[i].w = v
+		//fmt.Println(forest[i])
 		i++
 	}
 	return forest
 }
 
-func combineTrees(t1 tree, t2 tree) tree {
-	var t3 tree
-	t3.weight = t1.weight + t2.weight
+func combineTrees(t1 tree, t2 tree) *tree {
+	var t3 *tree = &tree{}
+	t3.w = t1.w + t2.w
 	t3.l = &t1
 	t3.r = &t2
 	return t3
 }
 
-func makeTree(forest []tree) tree {
-	var newTree tree
+func makeTree(forest []tree) *tree {
+	var newTree *tree
 	for len(forest) > 1 {
-		sort.Slice(forest, func(i, j int) bool { return forest[i].weight < forest[j].weight })
+		sort.Slice(forest, func(i, j int) bool { return forest[i].w < forest[j].w })
 		newTree = combineTrees(forest[0], forest[1])
 		forest[1] = newTree
 		forest = forest[1:]
 	}
 	return newTree
+}
+
+func idTree(t *tree, i int) {
+	i++
+	if t.l != nil {
+		idTree(t.l, i)
+	}
+	t.id = i
+	if t.r != nil {
+		idTree(t.r, i)
+	}
+}
+
+func treeToString(root *tree, s string) string {
+	if root.l != nil {
+		s = treeToString(root.l, s)
+	}
+	id := fmt.Sprintf("%08b", root.id)
+	char := fmt.Sprintf("%08b", root.c)
+	s = s + id + char
+	if root.l != nil {
+		left := fmt.Sprintf("%08b", root.l.id)
+		s += left
+	} else {
+		left := fmt.Sprintf("%08b", 0)
+		s += left
+	}
+	if root.r != nil {
+		right := fmt.Sprintf("%08b", root.r.id)
+		s += right
+	} else {
+		right := fmt.Sprintf("%08b", 0)
+		s += right
+	}
+	if root.r != nil {
+		s = treeToString(root.r, s)
+	}
+	return s
 }
 
 func storeLeaves(root *tree) []rune {
@@ -65,7 +104,7 @@ func storeLeaves(root *tree) []rune {
 			s = append(s, storeLeaves(root.l)...)
 		}
 		if root.l == nil && root.r == nil {
-			s = append(s, root.char)
+			s = append(s, root.c)
 		}
 		if root.r != nil {
 			s = append(s, storeLeaves(root.r)...)
@@ -82,7 +121,7 @@ func traverse(root *tree) []rune {
 		if root.l != nil {
 			s = append(s, traverse(root.l)...)
 		}
-		s = append(s, root.char)
+		s = append(s, root.c)
 		if root.r != nil {
 			s = append(s, traverse(root.r)...)
 		}
@@ -95,8 +134,8 @@ func paths(t *tree, path string) map[rune]string {
 		return nil
 	}
 	result := merge(paths(t.l, path+"0"), paths(t.r, path+"1"))
-	if t.char != 0 {
-		result[t.char] = path
+	if t.c != 0 {
+		result[t.c] = path
 	}
 	return result
 }
@@ -128,7 +167,7 @@ func makeTable(content string) map[rune]string {
 
 func stringToBits(s string, m map[rune]string) []byte {
 	b := make([]byte, 1)
-	eof := "1111"
+	eof := m['ൾ']
 	var offset, i int
 	for _, runeValue := range s {
 		bitSequence := m[runeValue]
@@ -159,7 +198,7 @@ func stringToBits(s string, m map[rune]string) []byte {
 		if bit == '0' {
 			b[i] <<= 1
 			offset++
-		} else if bit == '1' {
+		} else {
 			b[i] <<= 1
 			b[i] |= 1
 			offset++
@@ -169,22 +208,83 @@ func stringToBits(s string, m map[rune]string) []byte {
 	return b
 }
 
+func treeStringToBits(s string) []byte {
+	//fmt.Printf("%s\n", s)
+	b := make([]byte, 1)
+	var offset, i int
+	for _, bit := range s {
+		if offset == 8 {
+			offset = 0
+			var new byte
+			b = append(b, new)
+			i++
+		}
+		if bit == '0' {
+			b[i] <<= 1
+			offset++
+		} else if bit == '1' {
+			b[i] <<= 1
+			b[i] |= 1
+			offset++
+		}
+	}
+	return b
+}
+
 func compress(fileName string) {
 	f, err := os.Open(fileName)
 	check(err)
 	defer f.Close()
 	b, err := ioutil.ReadFile(fileName)
 	check(err)
-	data := string(b)
-	table := makeTable(data)
-	compressedData := stringToBits(data, table)
+	data := string(b) + "ൾ"
+	m := countChars(data)
+	forest := makeForest(m)
+	t := makeTree(forest)
+	var treeString string
+	treeString := treeToString(t, treeString)
+	compressedTree := treeStringToBits(treeString)
+	var path string
+	table := paths(&t, path)
 	compressedFileName := fmt.Sprintf("%s.huff", fileName)
 	cF, err := os.Create(compressedFileName)
 	check(err)
 	defer cF.Close()
+	compressedData := append(compressedTree, stringToBits(data, table)...)
 	n, err := cF.Write(compressedData)
 	fmt.Printf("%v bytes written", n)
 	check(err)
+}
+
+func bitsToTree(b []byte) []*tree {
+	treeFields := make([]treeInfo, 0)
+	m := make(map[int]*tree)
+	for i := 0; i < len(b); i += 4 {
+		var ti treeInfo
+		var t tree
+		ti.id = int(b[i])
+		t.id = int(b[i])
+		ti.c = int32(b[i+1])
+		t.c = int32(b[i+1])
+		ti.l = int(b[i+2])
+		ti.r = int(b[i+3])
+		treeFields = append(treeFields, ti)
+		m[t.id] = &t
+	}
+	fmt.Printf("%v\n", m)
+	fmt.Printf("%v\n", treeFields)
+	newTree := make([]*tree, 0)
+	for i := 0; i < len(treeFields); i++ {
+		t := m[treeFields[i].id]
+		if tl, ok := m[treeFields[i].l]; ok {
+			t.l = tl
+		}
+		if tr, ok := m[treeFields[i].r]; ok {
+			t.r = tr
+		}
+		newTree = append(newTree, t)
+	}
+	return newTree
 }
 
 func main() {
@@ -201,5 +301,8 @@ func main() {
 		fmt.Printf("%c: %s \n", c, p)
 	}
 	*/
-	compress("testFile")
+	fmt.Println("Enter the name of the file to compress:")
+	var fileName string
+	fmt.Scanln(&fileName)
+	compress(fileName)
 }
